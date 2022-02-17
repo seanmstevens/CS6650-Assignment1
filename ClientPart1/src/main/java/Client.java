b1faa18519f7d811b47e751aa8beafe917de8a53
Sean Stevens <sstevens88@gmail.com>
2022-02-17 13:14:44 -0800

    refactored client code

--- ClientPart1/src/main/java/Client.java
+++ ClientPart1/src/main/java/Client.java
@@ -5,14 +5,15 @@ import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.CyclicBarrier;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
-import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicInteger;
 
 public class Client {
 
   protected static final AtomicInteger NUM_SUCCESSFUL = new AtomicInteger(0);
   protected static final AtomicInteger NUM_FAILED = new AtomicInteger(0);
-  private static final Integer DAY_LENGTH = 420;
+  private static Integer numSkiers;
+  private static Integer numLifts;
+  private static String serverUrl;
   private static ExecutorService pool;
   private static CyclicBarrier barrier;
 
@@ -20,50 +21,61 @@ public class Client {
     Args opts = new Args();
     JCommander.newBuilder().addObject(opts).build().parse(args);
 
-    int t = opts.getNumThreads(),
-        s = opts.getNumSkiers(),
-        l = opts.getNumLifts(),
-        r = opts.getNumRuns();
+    int t = opts.getNumThreads();
+    int numRuns = opts.getNumRuns();
+    numSkiers = opts.getNumSkiers();
+    numLifts = opts.getNumLifts();
 
     InetSocketAddress address = opts.getAddress();
-    String serverUrl = "http://" + address.getHostName() + ":" + address.getPort() + "/Server_war";
+    serverUrl = "http://" + address.getHostName() + ":" + address.getPort() + "/Server_war";
 
     int phaseOneThreads = t / 4;
     int phaseThreeThreads = t / 10;
+    int totalThreads = phaseOneThreads + t + phaseThreeThreads;
 
-    pool = Executors.newFixedThreadPool(phaseOneThreads + t + phaseThreeThreads);
-    barrier = new CyclicBarrier(phaseOneThreads + t + phaseThreeThreads);
+    pool = Executors.newFixedThreadPool(totalThreads);
+    barrier = new CyclicBarrier(totalThreads);
 
     PhaseOptions p1Opts =
-        new PhaseOptions(phaseOneThreads, s, l, r, serverUrl, 0.2, 1, 90, (int) ((r * 0.2) * (s / (phaseOneThreads))));
+        new PhaseOptions(
+            "Phase One",
+            phaseOneThreads,
+            0.2,
+            1,
+            90,
+            (int) ((numRuns * 0.2) * (numSkiers / (phaseOneThreads))));
 
     PhaseOptions p2Opts =
-        new PhaseOptions(t, s, l, r, serverUrl, 0.2, 91, 360, (int) ((r * 0.6) * (s / t)));
+        new PhaseOptions("Phase Two", t, 0.2, 91, 360, (int) ((numRuns * 0.6) * (numSkiers / t)));
 
     PhaseOptions p3Opts =
-        new PhaseOptions(phaseThreeThreads, s, l, r, serverUrl, 1.0, 361, 420, (int) ((r * 0.1)));
+        new PhaseOptions("Phase Three", phaseThreeThreads, 1.0, 361, 420, (int) ((numRuns * 0.1)));
 
     long start = System.currentTimeMillis();
     executePhase(p1Opts);
-    System.out.println("------ PHASE ONE COMPLETE ------");
-
+    System.out.println(Thread.activeCount());
     executePhase(p2Opts);
-    System.out.println("------ PHASE TWO COMPLETE ------");
-
+    System.out.println(Thread.activeCount());
     executePhase(p3Opts);
-    System.out.println("------ PHASE THREE COMPLETE ------");
+    System.out.println(Thread.activeCount());
 
     barrier.await();
+    pool.shutdown();
     long end = System.currentTimeMillis();
 
     System.out.println("Total successes: " + NUM_SUCCESSFUL);
     System.out.println("Total failures: " + NUM_FAILED);
     System.out.println("Time elapsed (sec): " + ((float) (end - start) / 1000));
+    System.out.println(
+        "Total throughput (reqs/sec): "
+            + (NUM_SUCCESSFUL.get() + NUM_FAILED.get()) / ((float) (end - start) / 1000));
+    System.out.println(Thread.activeCount());
   }
 
   public static void executePhase(PhaseOptions opts) throws InterruptedException {
+    System.out.println("------ " + opts.getName().toUpperCase() + " INITIALIZED ------");
     int t = opts.getNumThreads();
-    int s = opts.getNumSkiers();
+    int s = numSkiers;
 
     CountDownLatch latch = new CountDownLatch((int) Math.ceil(t * opts.getThreshold()));
 
@@ -77,13 +89,19 @@ public class Client {
               endId,
               opts.getStartTime(),
               opts.getEndTime(),
-              opts.getServerUrl(),
+              serverUrl,
               opts.getNumReqs(),
-              opts.getNumLifts(),
+              numLifts,
               latch,
               barrier));
     }
 
     latch.await();
+    System.out.println(
+        "------ "
+            + opts.getName().toUpperCase()
+            + ": "
+            + ((int) (opts.getThreshold() * 100))
+            + "% OF THREADS COMPLETE ------");
   }
 }
--- ClientPart1/src/main/java/PhaseOptions.java
+++ ClientPart1/src/main/java/PhaseOptions.java
@@ -1,54 +1,33 @@
 public class PhaseOptions {
 
+  private final String name;
   private final Integer numThreads;
-  private final Integer numSkiers;
-  private final Integer numLifts;
-  private final Integer numRuns;
-  private final String serverUrl;
   private final Double threshold;
   private final Integer startTime;
   private final Integer endTime;
   private final Integer numReqs;
 
   public PhaseOptions(
+      String name,
       Integer numThreads,
-      Integer numSkiers,
-      Integer numLifts,
-      Integer numRuns,
-      String serverUrl,
       Double threshold,
       Integer startTime,
       Integer endTime,
       Integer numReqs) {
+    this.name = name;
     this.numThreads = numThreads;
-    this.numSkiers = numSkiers;
-    this.numLifts = numLifts;
-    this.numRuns = numRuns;
-    this.serverUrl = serverUrl;
     this.threshold = threshold;
     this.startTime = startTime;
     this.endTime = endTime;
     this.numReqs = numReqs;
   }
 
+  public String getName() {
+    return name;
+  }
+
   public Integer getNumThreads() {
     return numThreads;
-  }
-
-  public Integer getNumSkiers() {
-    return numSkiers;
-  }
-
-  public Integer getNumLifts() {
-    return numLifts;
-  }
-
-  public Integer getNumRuns() {
-    return numRuns;
-  }
-
-  public String getServerUrl() {
-    return serverUrl;
   }
 
   public Double getThreshold() {
